7a166334c87aa72398a5cbce54902dbc4f71dd2d
JaymonstreMain <89759962+JaymonstreMain@users.noreply.github.com>
2022-02-03 12:25:56 -0500

    Update RectangleTest.java

--- try/src/tests/RectangleTest.java
+++ try/src/tests/RectangleTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.*;
 public class RectangleTest {
     private Rectangle rectangle1, rectangle2, rectangle3, rectangle4, rectangle5, rectangle6;
 
+
+
     @Before
     public void setUp() throws Exception {
         rectangle1 = new Rectangle(1, 2);
@@ -21,6 +23,7 @@ public class RectangleTest {
 
     @Test
     public void calculerPerimetre() {
+
     }
 
     @Test
@@ -33,22 +36,48 @@ public class RectangleTest {
 
     @Test
     public void getCouleur() {
+        assertEquals(rectangle1.getCouleur(), "rouge");
+        assertEquals(rectangle2.getCouleur(), "rouge");
+        assertEquals(rectangle3.getCouleur(), "rouge");
+        assertEquals(rectangle4.getCouleur(), "rouge");
+        assertEquals(rectangle5.getCouleur(), "rouge");
+        assertEquals(rectangle6.getCouleur(), "rouge");
+
     }
 
     @Test
     public void getHauteur() {
+        assertEquals(rectangle1.getHauteur(), 1);
+        assertEquals(rectangle2.getHauteur(), 11);
+        assertEquals(rectangle3.getHauteur(), 14);
+        assertEquals(rectangle4.getHauteur(), 6);
+        assertEquals(rectangle5.getHauteur(), 13);
+        assertEquals(rectangle6.getHauteur(), 14);
     }
 
     @Test
     public void getLargeur() {
+        assertEquals(rectangle1.getLargeur(), 2);
+        assertEquals(rectangle2.getLargeur(), 9);
+        assertEquals(rectangle3.getLargeur(), 27);
+        assertEquals(rectangle4.getLargeur(), 12);
+        assertEquals(rectangle5.getLargeur(), 8);
+        assertEquals(rectangle6.getLargeur(), 9);
     }
 
     @Test
     public void getNom() {
+        assertEquals(rectangle1.getNom(), "Rectangle");
+        assertEquals(rectangle2.getNom(), "Rectangle");
+        assertEquals(rectangle3.getNom(), "Rectangle");
+        assertEquals(rectangle4.getNom(), "Rectangle");
+        assertEquals(rectangle5.getNom(), "Rectangle");
+        assertEquals(rectangle6.getNom(), "Rectangle");
     }
 
     @Test
     public void setCouleur() {
+
     }
 
     @Test
